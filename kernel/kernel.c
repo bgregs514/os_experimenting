dbb933c311fb3bcda8710d0b10b036b38e5b64aa
Ben Gregory <TBD>
2020-07-10 11:20:17 -0500

    kmalloc implemented

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,7 @@
 #include "../drivers/screen.h"
 #include "kernel.h"
 #include "../libc/string.h"
+#include "../libc/mem.h"
 
 void main()
 {
@@ -17,6 +18,18 @@ void user_input(char *input)
 	if (strcmp(input, "END") == 0) {
 		kprint("\nStopping the CPU");
 		asm volatile("hlt");
+	} else if (strcmp(input, "PAGE") == 0) {
+		u32 phys_addr;
+		u32 page = kmalloc(1000, 1, &phys_addr);
+		char page_str[16] = "";
+		hex_to_ascii(page, page_str);
+		char phys_str[16] = "";
+		hex_to_ascii(phys_addr, phys_str);
+		kprint("Page: ");
+		kprint(page_str);
+		kprint(", pysical address: ");
+		kprint(phys_str);
+		kprint("\n");
 	}
 	kprint("You entered: ");
 	kprint(input);
--- libc/mem.c
+++ libc/mem.c
@@ -13,3 +13,24 @@ void memory_set(u8 *dest, u8 val, u32 len)
 	for (; len != 0; len--)
 		*temp++ = val;
 }
+
+/* this shouldn't be hardcoded and needs to be updated;
+ just pick a safe place outside of the kernel loading point
+ of 0x1000 */
+u32 free_mem_addr = 0x10000;
+
+u32 kmalloc(u32 size, int align, u32 *phys_addr)
+{
+	/* align pages to 4k/0x1000 */
+	if (align == 1 && (free_mem_addr & 0xFFFFF000)) {
+		free_mem_addr &= 0xFFFFF000;
+		free_mem_addr += 0x1000;
+	}
+	/* save the physical address */
+	if (phys_addr)
+		*phys_addr = free_mem_addr;
+
+	u32 ret = free_mem_addr;
+	free_mem_addr += size; /* increment the pointer */
+	return ret;
+}
